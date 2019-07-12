ae68df738f5592033288b6942910820f7e2396bd
Gökhan Uçar <gokhanucar@pasyazilim.com>
2019-07-12 18:52:34 +0300

    Access tokens added

--- routes/web.php
+++ routes/web.php
@@ -32,6 +32,7 @@ Route::post('password/email', 'Auth\ForgotPasswordController@showResetLinkEmail'
 Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
 Route::post('password/reset', 'Auth\ResetPasswordController@reset');
 
+Route::get('/home/my-tokens', 'HomeController@getTokens')->name('personal-tokens');
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/', function() {
